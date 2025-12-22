6f944c59ea005019ee3b1d848b4436cf0b46b797
simon3397 <simon3397@gmail.com>
2025-12-23 01:41:13 +0900

    [FEAT] navigation route 추가 일정 실행 중지 화면, 일정실행 조기 완료 화면, 방해금지모드 실행중 화면

--- core/src/main/java/com/miruni/core/navigation/MiruniRoute.kt
+++ core/src/main/java/com/miruni/core/navigation/MiruniRoute.kt
@@ -5,6 +5,13 @@ sealed class MiruniRoute(val route: String) {
     data object Home : MiruniRoute("home")
     data object HomeDndOnboarding : MiruniRoute("home/dnd/onboarding")
     data object HomeDndTimerSetting : MiruniRoute("home/dnd/timerSetting")
+    data object HomeDndPause : MiruniRoute("home/dnd/pause")
+    data object HomeDndEarlyEnd : MiruniRoute("home/dnd/earlyEnd")
+    data object HomeDndTimerRunning : MiruniRoute("home/dnd/timerRunning/{hour}/{minute}") {
+        fun createRoute(hour: Int, minute: Int) : String {
+            return "home/dnd/timerRunning/$hour/$minute"
+        }
+    }
 
     data object Calendar : MiruniRoute("calendar")
     data object MyPage : MiruniRoute("myPage")
--- feature/home/src/main/kotlin/com/miruni/feature/home/navigation/HomeNavigation.kt
+++ feature/home/src/main/kotlin/com/miruni/feature/home/navigation/HomeNavigation.kt
@@ -1,12 +1,19 @@
 package com.miruni.feature.home.navigation
 
+import android.util.Log
+import androidx.compose.material3.TimePickerState
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.miruni.core.navigation.MiruniRoute
 import com.miruni.core.navigation.NavigationDestination
 import com.miruni.feature.home.HomeScreen
+import com.miruni.feature.home.dnd.DndEarlyEndScreen
 import com.miruni.feature.home.dnd.DndOnboardingScreen
+import com.miruni.feature.home.dnd.DndPauseScreen
+import com.miruni.feature.home.dnd.DndTimerRunningScreen
 import com.miruni.feature.home.dnd.DndTimerSetScreen
 import jakarta.inject.Inject
 
@@ -19,20 +26,59 @@ class HomeNavigation @Inject constructor(
         builder: NavGraphBuilder,
         navController: NavHostController
     ) {
+        Log.d("HomeNavigation", "Register HomeNavigation")
+
         builder.composable(MiruniRoute.Home.route) {
-            HomeScreen(navController = navController)
+            Log.d("HomeNavigation", "HomeScreen entered")
+            HomeScreen(
+                navController = navController
+            )
         }
 
         builder.composable(MiruniRoute.HomeDndOnboarding.route) {
+            Log.d("HomeNavigation", "DndOnboardingScreen entered")
             DndOnboardingScreen(
                 navController = navController
             )
         }
 
         builder.composable(MiruniRoute.HomeDndTimerSetting.route) {
+            Log.d("HomeNavigation", "HomeDndTimerSetting entered")
             DndTimerSetScreen(
                 navController = navController,
-                onConfirm = { _, _ -> }
+            )
+        }
+
+        builder.composable(MiruniRoute.HomeDndPause.route) {
+            Log.d("HomeNavigation", "HomeDndPause entered")
+            DndPauseScreen(
+                navController = navController,
+            )
+        }
+
+        builder.composable(MiruniRoute.HomeDndEarlyEnd.route) {
+            Log.d("HomeNavigation", "HomeDndEarlyEnd entered")
+            DndEarlyEndScreen(
+                navController = navController,
+            )
+        }
+
+        builder.composable(
+            MiruniRoute.HomeDndTimerRunning.route,
+            arguments = listOf(
+                navArgument("hour") { type = NavType.IntType},
+                navArgument("minute") { type = NavType.IntType}
+            )
+        ) { backStackEntry ->
+
+            val hour = backStackEntry.arguments?.getInt("hour") ?: 0
+            val minute = backStackEntry.arguments?.getInt("minute") ?: 0
+
+            Log.d("HomeNavigation", "HomeDndTimerRunning entered")
+            DndTimerRunningScreen(
+                hour = hour,
+                minute = minute,
+                navController = navController,
             )
         }
     }
