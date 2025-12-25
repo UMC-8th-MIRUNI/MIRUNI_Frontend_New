0f3a0cd5e5508d20981f44711a1047fb602ddb55
Junho Cho <hiwhwnsgh@gmail.com>
2025-12-25 21:25:51 +0900

    feat(pwreset) : 기본 배경 구현

---
+++ feature/pwreset/src/main/kotlin/com/miruni/feature/pwreset/component/screen/PwResetCheckScreen.kt
@@ -0,0 +1,12 @@
+package com.miruni.feature.pwreset.component.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PwResetCheckScreen(
+    onPrevClicked: () -> Unit,
+    onNextClicked: () -> Unit,
+
+) {
+
+}
---
+++ feature/pwreset/src/main/kotlin/com/miruni/feature/pwreset/component/screen/PwResetEmailScreen.kt
@@ -0,0 +1,11 @@
+package com.miruni.feature.pwreset.component.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PwResetEmailScreen(
+    onPrevClicked: () -> Unit,
+    onNextClicked: () -> Unit,
+) {
+
+}
