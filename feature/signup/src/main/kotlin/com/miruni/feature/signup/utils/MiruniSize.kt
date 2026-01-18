894453f7196cb3d4ab8959d4eb562746aa784d6f
Junho Cho <hiwhwnsgh@gmail.com>
2026-01-18 14:58:02 +0900

    fix(pwreset) : navigation 코드 수정

---
+++ feature/signup/src/main/kotlin/com/miruni/feature/signup/utils/MiruniSize.kt
@@ -0,0 +1,16 @@
+package com.miruni.feature.signup.utils
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+object MiruniSize {
+    val iconSm: Dp = 16.dp
+    val iconMd: Dp = 24.dp
+    val iconLg: Dp = 32.dp
+
+    val buttonWidth : Dp = 70.dp
+    val buttonHeight : Dp = 30.dp
+
+    val height : Dp = 50.dp
+    val heightSmall : Dp = 40.dp
+}
