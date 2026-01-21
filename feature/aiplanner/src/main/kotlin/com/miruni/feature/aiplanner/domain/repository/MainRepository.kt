819d56413b669612d60cf8a4cf301214cf9445a5
Lee-CH810 <2002ckdghks@naver.com>
2026-01-21 23:14:18 +0900

    [FIX] domain/repository 수정(스웨거 기준)

--- feature/aiplanner/src/main/kotlin/com/miruni/feature/aiplanner/domain/repository/MainRepository.kt
+++ feature/aiplanner/src/main/kotlin/com/miruni/feature/aiplanner/domain/repository/MainRepository.kt
@@ -2,14 +2,12 @@ package com.miruni.feature.aiplanner.domain.repository
 
 import com.miruni.core.result.DataError
 import com.miruni.core.result.DataResult
-import com.miruni.feature.aiplanner.domain.model.PlanProgress
+import com.miruni.feature.aiplanner.domain.model.PlanPreview
 
 interface MainRepository {
     /** AI 상위 일정 조회
-     * - AI 일정들 가져오기
+     * - AI 플래닝 잔여 횟수
+     * - AI 일정(상위)
      */
-    suspend fun getAiPlans(): DataResult<List<PlanProgress>, DataError>
-
-    /** AI 플래닝 잔여 횟수 가져오기 */
-    suspend fun getRemain(): DataResult<Int, DataError>
+    suspend fun getAiPlans(): DataResult<List<PlanPreview>, DataError>
 }
