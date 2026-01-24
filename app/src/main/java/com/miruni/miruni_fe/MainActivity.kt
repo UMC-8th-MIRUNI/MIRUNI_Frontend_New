0775ff835425c9055f486ed502b9cffb487ae6db
Lee-CH810 <2002ckdghks@naver.com>
2026-01-25 02:25:08 +0900

    [FEAT] MainActivity: 시스템 기본 스플래시 관련 설정 - 앱 준비 완료 시점까지 시스템 기본 스플래시 출력(현재 온보딩 컬러로 모두 채움) - 추후 initializing 필요할 시, 이때 준비하게 할 수 있음

--- app/src/main/java/com/miruni/miruni_fe/MainActivity.kt
+++ app/src/main/java/com/miruni/miruni_fe/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshotFlow
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.miruni.core.designsystem.MiruniTheme
 import com.miruni.core.navigation.NavigationDestination
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.first
+import java.util.concurrent.atomic.AtomicBoolean
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -18,14 +23,20 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var destinations: Set<@JvmSuppressWildcards NavigationDestination>
 
+    // 스플래시 얼마나 유지할지
+    private val keepSplashOn = AtomicBoolean(true)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
+        // 앱 준비 완료 시점까지 시스템 기본 스플래시 유지
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition { keepSplashOn.get() }
+
+        enableEdgeToEdge()
 //        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v, windowInsets ->
 //            WindowInsetsCompat.CONSUMED
 //        }
-
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         val controller = WindowCompat.getInsetsController(window, window.decorView)
@@ -38,6 +49,10 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             MiruniTheme {
+                LaunchedEffect(Unit) {
+                    snapshotFlow { true }.first()
+                    keepSplashOn.set(false)
+                }
                 MainScreen(destinations = destinations)
             }
         }
