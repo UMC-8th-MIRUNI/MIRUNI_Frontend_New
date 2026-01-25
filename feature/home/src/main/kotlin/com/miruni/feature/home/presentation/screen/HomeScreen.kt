28c5142e1d8747a0b0d5fb48d789b5a700731bd4
simon3397 <simon3397@gmail.com>
2026-01-25 19:40:03 +0900

    feat(home-dnd) : 방해금지모드, 일정실행 navigation 연결

--- feature/home/src/main/kotlin/com/miruni/feature/home/presentation/screen/HomeScreen.kt
+++ feature/home/src/main/kotlin/com/miruni/feature/home/presentation/screen/HomeScreen.kt
@@ -75,8 +75,8 @@ fun HomeScreen(
                 HomeContract.Effect.Navigation.ToAiPlannerOnboarding -> navController.navigate(MiruniRoute.AiPlannerOnboarding.route) // AI 플래너 온보딩
                 HomeContract.Effect.Navigation.ToAiPlanner -> navController.navigate(MiruniRoute.AiPlannerMain.route) // AI 플래너
                 HomeContract.Effect.Navigation.ToAlarms -> navController.navigate(MiruniRoute.AlarmLogs.route) // 알람 기록
-                HomeContract.Effect.Navigation.ToDnd -> navController.navigate(MiruniRoute.Dnd.route) // 방해금지 모드
-                is HomeContract.Effect.Navigation.ToExecution -> navController.navigate(MiruniRoute.Execution.route) // 일정 실행
+                HomeContract.Effect.Navigation.ToDnd -> navController.navigate(MiruniRoute.HomeDndTimerSetting.route) // 방해금지 모드 타이머
+                is HomeContract.Effect.Navigation.ToExecution -> navController.navigate(MiruniRoute.RunScheduleTimerSetting.route) // 일정 실행 타이머
                 is HomeContract.Effect.ShowToast -> Toast.makeText(context, effect.message, Toast.LENGTH_SHORT).show() // 토스트 출력
                 else -> {}
             }
