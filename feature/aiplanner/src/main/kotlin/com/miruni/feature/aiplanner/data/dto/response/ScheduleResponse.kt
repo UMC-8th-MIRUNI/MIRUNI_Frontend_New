28a5fc6a9b89cfbe17ff2a051aba77dc46093f1e
Lee-CH810 <2002ckdghks@naver.com>
2026-01-26 18:14:44 +0900

    [FIX] AI 플래닝하여 서버로부터 받는 응답이 정의된 data class와 달라서 발생하는 런타임 에러 해결

--- feature/aiplanner/src/main/kotlin/com/miruni/feature/aiplanner/data/dto/response/ScheduleResponse.kt
+++ feature/aiplanner/src/main/kotlin/com/miruni/feature/aiplanner/data/dto/response/ScheduleResponse.kt
@@ -4,18 +4,23 @@ import com.miruni.feature.aiplanner.domain.model.Plan
 import com.miruni.feature.aiplanner.domain.model.PlanPriority
 
 data class ScheduleResponse(
-    val plan: PlanDto,
+    val planId: Long, // 상위 일정 id
+    val title: String, // 상위 일정 제목
+    val deadline: String, // 마감 기한
+    val taskRange: String, // 일정 범위
+    val priority: String, // 우선 순위
     val progressPercentage: Int, // 진행률
+    val aiPlans: List<AiPlanDto> // 매칭되는 하위 일정
 ) {
     fun toDomain(): Plan {
         return Plan(
-            planId = plan.planId,
-            title = plan.title,
-            deadline = plan.deadline,
-            taskRange = plan.taskRange,
-            priority = PlanPriority.fromServer(plan.priority),
+            planId = planId,
+            title = title,
+            deadline = deadline,
+            taskRange = taskRange,
+            priority = PlanPriority.fromServer(priority),
             progressRate = progressPercentage.toLong(),
-            aiPlans = plan.aiPlans.map { it.toDomain() }
+            aiPlans = aiPlans.map { it.toDomain() }
         )
     }
 }
