397fe0536ea7d3db8935d680fbe82c002c71547a
simon3397 <simon3397@gmail.com>
2026-02-10 22:05:20 +0900

    feat(survey) : resolve conflict

--- feature/home/src/main/kotlin/com/miruni/feature/home/presentation/screen/HomeScreen.kt
+++ feature/home/src/main/kotlin/com/miruni/feature/home/presentation/screen/HomeScreen.kt
@@ -1,9 +1,10 @@
-package com.miruni.feature.home.presentation
+package com.miruni.feature.home.presentation.screen
 
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
