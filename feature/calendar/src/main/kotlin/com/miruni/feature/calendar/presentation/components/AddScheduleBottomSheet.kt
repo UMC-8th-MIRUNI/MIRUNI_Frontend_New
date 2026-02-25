6abe8eb4f6cc09fcd6de456134a7e2eacd0ace81
Lee-CH810 <2002ckdghks@naver.com>
2026-02-26 03:24:39 +0900

    [FEAT] 일정 생성 바텀 시트: - 우선순위 드롭다운 누락되어 추가

--- feature/calendar/src/main/kotlin/com/miruni/feature/calendar/presentation/components/AddScheduleBottomSheet.kt
+++ feature/calendar/src/main/kotlin/com/miruni/feature/calendar/presentation/components/AddScheduleBottomSheet.kt
@@ -7,16 +7,24 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
@@ -29,11 +37,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.miruni.core.designsystem.AppTypography
 import com.miruni.core.designsystem.Black
 import com.miruni.core.designsystem.Gray
+import com.miruni.core.designsystem.MainColor
 import com.miruni.feature.calendar.common.MiruniButton
 import com.miruni.feature.calendar.common.MiruniTextField
 import com.miruni.feature.calendar.domain.model.PlanPriority
@@ -107,6 +121,14 @@ fun AddScheduleBottomSheet(
 
                 Spacer(modifier = Modifier.height(10.dp))
 
+                DropdownField(
+                    label = "우선순위",
+                    onPrioritySelected = { priority = it },
+                    selectedOption = priority
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+
                 DateField(
                     label = "날짜 및 시간",
                     dateTimeRange = dateTimeRange,
@@ -166,6 +188,129 @@ fun InputField(
             maxLines = maxLines,
             singleLine = singleLine
         )
+    }
+}
+
+@Composable
+fun DropdownField(
+    label: String,
+    modifier: Modifier = Modifier,
+    onPrioritySelected: (PlanPriority) -> Unit,
+    selectedOption: PlanPriority
+) {
+    Column {
+        Text(
+            text = label,
+            style = AppTypography.body_regular_14,
+            color = Black,
+            modifier = Modifier.padding(start = 4.dp, bottom = 8.dp)
+        )
+        val density = LocalDensity.current
+        var textFieldSize by remember { mutableStateOf(IntSize.Zero) }
+
+        var expanded by remember { mutableStateOf(false) }
+        val options = remember { PlanPriority.entries }
+
+        Box(modifier = modifier
+            .fillMaxWidth()
+            .height(41.dp)
+            .background(color = Color.White, shape = RoundedCornerShape(8.dp))
+            .border(
+                width = 2.dp,
+                color = if (expanded) MainColor.miruni_green else Gray.gray_400,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ) { expanded = true }
+            .padding(horizontal = 16.dp),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = modifier
+                    .fillMaxWidth()
+
+            ) {
+                BasicTextField(
+                    value = selectedOption.ui,
+                    onValueChange = {},
+                    readOnly = true,
+                    singleLine = true,
+                    textStyle = AppTypography.PretendardTextStyle(
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 13.sp,
+                        lineHeightRatio = 1f
+                    ),
+                    modifier = Modifier
+                        .weight(1f)
+                        .onGloballyPositioned { coordinates ->
+                            textFieldSize = coordinates.size
+                        }
+                )
+                Icon(
+                    imageVector = Icons.Default.ArrowDropDown,
+                    contentDescription = null,
+                    tint = Gray.gray_700
+                )
+            }
+
+            DropdownMenu(
+                expanded = expanded,
+                modifier = Modifier
+                    .width(
+                        with(density) { textFieldSize.width.toDp() }
+                    )
+                    .heightIn(max = 250.dp),
+                onDismissRequest = { expanded = false },
+                containerColor = Color.White,
+                tonalElevation = 0.dp
+            ) {
+                options.forEachIndexed { index, priority ->
+                    val isSelected = priority == selectedOption
+
+                    DropdownMenuItem(
+                        text = {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 8.dp),
+                                contentAlignment = Alignment.CenterStart
+                            ) {
+                                Text(
+                                    text = priority.ui,
+                                    style = AppTypography.PretendardTextStyle(
+                                        fontWeight = if (isSelected) FontWeight.Bold else FontWeight.Normal,
+                                        fontSize = 13.sp,
+                                        lineHeightRatio = 0.625f
+                                    ).copy(
+                                        color = if (isSelected) Color.White else Black
+                                    ),
+                                    textAlign = TextAlign.Center,
+                                    modifier = Modifier.padding(start = 16.dp)
+                                )
+                            }
+                        },
+                        contentPadding = PaddingValues(0.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(
+                                if (isSelected) MainColor.miruni_green
+                                else Color.White
+                            ),
+                        onClick = {
+                            onPrioritySelected(priority)
+                            expanded = false
+                        }
+                    )
+
+                    if (index != options.lastIndex) {
+                        HorizontalDivider(color = Gray.gray_300)
+                    }
+                }
+            }
+        }
     }
 }
 
