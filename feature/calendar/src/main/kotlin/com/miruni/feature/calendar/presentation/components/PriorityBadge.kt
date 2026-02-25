6594395ad98a6bf84dbcfc77001a5e08eb353d64
Lee-CH810 <2002ckdghks@naver.com>
2026-02-25 17:21:35 +0900

    [FIX] 패키지 이동 - presentation 계층 두기

--- feature/calendar/src/main/kotlin/com/miruni/feature/calendar/presentation/components/PriorityBadge.kt
+++ feature/calendar/src/main/kotlin/com/miruni/feature/calendar/presentation/components/PriorityBadge.kt
@@ -1,4 +1,4 @@
-package com.miruni.feature.calendar.components
+package com.miruni.feature.calendar.presentation.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
